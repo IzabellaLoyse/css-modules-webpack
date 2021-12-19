8e0efa7f90c40ca0b306bc74707100c5702a4b19
Izabella Loyse <izabellaloyse13@gmail.com>
2021-12-19 13:24:48 -0300

    feat: Finalizado o componente CardVideo

--- src/js/CardVideo.js
+++ src/js/CardVideo.js
@@ -2,17 +2,30 @@ import h from 'hyperscript';
 import helpers from 'hyperscript-helpers';
 import '../styles/card/card-global.css';
 import common from '../styles/card/card-common.css';
-import dogImg from '../assets/images/dog.jpg';
+import video from '../styles/card/card-video.css';
+import catImg from '../assets/images/cat.jpg';
 import firstAvatar from '../assets/images/photo-profile.jpeg';
 import iconLike from '../assets/icons/like.png';
 import iconComment from '../assets/icons/comment.png';
 import iconShare from '../assets/icons/share.png';
+import iconVideo from '../assets/icons/video.png';
+import iconPlay from '../assets/icons/play.png';
 
 const {
-  section, div, img, h3, p, article,
+  section, div, img, h3, p,
+  article,
 } = helpers(h);
 
-const photo = img({ className: common.cardImage, src: dogImg });
+const photo = img({ className: common.cardImage, src: catImg });
+const icon = img({ src: iconVideo });
+const iconContainer = div({ className: video.cardIcon }, [icon]);
+const play = img({ className: video.cardPlay, src: iconPlay });
+
+const photoContainer = section({ className: common.photoContainer }, [
+  photo,
+  iconContainer,
+  play,
+]);
 
 const avatar = img({ className: common.cardAvatar, src: firstAvatar });
 const name = h3({ className: common.cardTitle }, 'Izabella.loyse');
@@ -22,11 +35,18 @@ const like = img({ src: iconLike });
 const comment = img({ src: iconComment });
 const share = img({ src: iconShare });
 
-const actionIcons = div({ className: common.cardActionIcons }, [like, comment, share]);
-const contentHeader = div({ className: common.cardHeader }, [user, actionIcons]);
+const actionIcons = div({ className: common.cardActionIcons }, [
+  like,
+  comment,
+  share,
+]);
+const contentHeader = div({ className: common.cardHeader }, [
+  user,
+  actionIcons,
+]);
 
 const text = p(
-  'É maravilhoso ter um companheiro como você...muito obrigado por estar sempre ao meu lado  #dog #amorPorAnimais',
+  'É maravilhoso ter um companheiro como você...muito obrigado por estar sempre ao meu lado  #cat #amorPorAnimais',
 );
 const textContainer = div({ className: 'cardText' }, text);
 
@@ -35,6 +55,6 @@ const cardContent = article({ className: common.cardBody }, [
   textContainer,
 ]);
 
-const Card = section({ className: 'card' }, [photo, cardContent]);
+const Card = section({ className: 'card' }, [photoContainer, cardContent]);
 
 export default Card;
